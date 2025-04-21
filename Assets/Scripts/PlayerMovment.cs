c0b06d7393a2a84c9c8a423b755c6c3e9f327347
Anas_Jamjoum <anas13672@gmail.com>
2025-04-21 10:32:06 +0300

    Refactor player movement script to improve jump, moving ,fixed rotaion ,mechanics and streamline input handling

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -2,31 +2,64 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 10f; 
-    private Rigidbody2D rb; 
-    private Vector2 movement; 
+    [SerializeField] private float speed = 5; 
+    [SerializeField] private float jumpForce = 6f;
+
+    private Rigidbody2D body; 
     private Transform mainCamera; 
 
-    private void Start()
+    private int jumpCount = 0;
+
+    private int maxJumps = 2;
+
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody2D>();
 
         mainCamera = Camera.main.transform;
     }
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2( horizontalInput* speed, body.velocity.y);
 
         if (mainCamera != null)
         {
             mainCamera.position = new Vector3(transform.position.x, transform.position.y, mainCamera.position.z);
         }
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && jumpCount < maxJumps)
+        {
+            Jump();
+            jumpCount++;
+        }
     }
 
-    private void FixedUpdate()
+    private void Jump()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCount = 0;
+        }
+    }
+
 }
