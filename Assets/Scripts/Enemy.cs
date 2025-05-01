1ef07a3f0683db411019b2f7bc64c027fd4edcc6
Anas_Jamjoum <anas13672@gmail.com>
2025-05-01 17:21:28 +0300

    Refactor Enemy script to improve player detection and damage handling; change damage type to float and enhance Gizmo visualization

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,48 +3,65 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
-    [SerializeField] private int damage;
+    [SerializeField] private float damage;
     [SerializeField] private float range;
-    [SerializeField] private float colliderDistance;
-
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float ydistanceToPlayer = 5f;
     private float cooldownTimer = Mathf.Infinity;
+
+    private Health playerHealth;
 
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
+
         if (PlayerInSight())
         {
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
+                DamagePlayer();
             }
+        }
+        Debug.Log(PlayerInSight());
+    }
+
+private bool PlayerInSight()
+{
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    if (player != null)
+    {
+        float distanceToPlayerX = player.transform.position.x - transform.position.x;
+        float distanceToPlayerY = Mathf.Abs(player.transform.position.y - transform.position.y);
+
+        if (Mathf.Abs(distanceToPlayerX) <= range &&
+            Mathf.Sign(distanceToPlayerX) == Mathf.Sign(transform.localScale.x) &&
+            distanceToPlayerY <= ydistanceToPlayer)
+        {
+            playerHealth = player.GetComponent<Health>();
+            return true;
         }
     }
 
-    private bool PlayerInSight()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-        0,
-        Vector2.left,0,
-        playerLayer);
-        return hit.collider != null; 
-    }
+    return false;
+}
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x  * colliderDistance,
-        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
-    }
+private void OnDrawGizmos()
+{
+    Gizmos.color = Color.red;
+    Vector3 center = transform.position + Vector3.right * range / 2 * (transform.localScale.x / Mathf.Abs(transform.localScale.x));
+    Vector3 size = new Vector3(range, ydistanceToPlayer * 2, 0);
+    Gizmos.DrawWireCube(center, size);
+}
 
     private void DamagePlayer()
     {
         if (PlayerInSight())
         {
-            
+            playerHealth.TakeDamage(damage);
+            cooldownTimer = 0;
         }
     }
 }
