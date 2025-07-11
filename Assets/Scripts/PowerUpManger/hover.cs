959e99bba87cd2dd862d6f6eccaaaf88c685d2bc
diyabakri <dbakree@gmail.com>
2025-07-11 19:57:15 +0300

    added hover to power up

---
+++ Assets/Scripts/PowerUpManger/hover.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour
+{
+    public float amplitud = 0.002f;    
+    public float hoverSpeed = 1.2f;    
+    void Update()
+    {
+        Vector3 hoverPosition = transform.position;
+        hoverPosition.y += amplitud * Mathf.Cos(Time.time * hoverSpeed);
+        transform.position = hoverPosition;
+    }
+}
