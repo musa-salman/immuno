c82869d8f660f21cf51d200c903c5de363fc2219
musa-salman <musa.salman.g1@gmail.com>
2025-07-14 23:17:36 +0300

    Fix unassigned loading screen.

--- Assets/Scripts/Core/SceneController.cs
+++ Assets/Scripts/Core/SceneController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class SceneController : MonoBehaviour {
+public class SceneController : MonoBehaviour
+{
     [SerializeField] GameObject loadingScreen;
     public static SceneController Instance;
     public CanvasGroup fadeCanvasGroup;
@@ -12,24 +13,36 @@ public class SceneController : MonoBehaviour {
     public Action OnSceneTransitionStart;
     public Action OnSceneTransitionEnd;
 
-    private void Awake() {
-        if (Instance == null) {
+    private void Awake()
+    {
+        if (Instance == null)
+        {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        } else {
+        }
+        else
+        {
             Destroy(gameObject);
         }
     }
 
-    public void LoadScene(string sceneName, Action onComplete = null) {
+    private void Start()
+    {
+        loadingScreen.SetActive(false);
+    }
+
+    public void LoadScene(string sceneName, Action onComplete = null)
+    {
         StartCoroutine(LoadSceneCoroutine(sceneName, onComplete));
     }
 
-    public IEnumerator LoadSceneCoroutine(string sceneName, Action onComplete = null) {
+    public IEnumerator LoadSceneCoroutine(string sceneName, Action onComplete = null)
+    {
         OnSceneTransitionStart?.Invoke();
         yield return FadeOut(fadeDuration);
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         while (!asyncLoad.isDone)
         {
 
@@ -42,18 +55,22 @@ public class SceneController : MonoBehaviour {
         onComplete?.Invoke();
     }
 
-    private IEnumerator FadeOut(float duration) {
+    private IEnumerator FadeOut(float duration)
+    {
         float timer = 0f;
-        while (timer < duration) {
+        while (timer < duration)
+        {
             timer += Time.deltaTime;
             fadeCanvasGroup.alpha = timer / duration;
             yield return null;
         }
     }
 
-    private IEnumerator FadeIn(float duration) {
+    private IEnumerator FadeIn(float duration)
+    {
         float timer = 0f;
-        while (timer < duration) {
+        while (timer < duration)
+        {
             timer += Time.deltaTime;
             fadeCanvasGroup.alpha = 1 - (timer / duration);
             yield return null;
